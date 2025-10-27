3550b1375fc48749d63e607b50e1317a35006c6e
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-10-27 10:54:07 -0300

    test_fix

--- tb/testbench_funcional.sv
+++ tb/testbench_funcional.sv
@@ -11,29 +11,21 @@ module tb;
     realtime    rtime1,    rtime2;
     parameter BYTESIZES = 8, OVERSAMPLING = 16, BAUDRATE = 9600,COUNTER_CLOCK_INPUT = 50_000_000,COUNTER_CLOCK_INPUT2 = 100_000_000, CLOCK_REF = 5_000_000;
 
-    uart_rx #(.BYTESIZES(BYTESIZES), .OVERSAMPLING(OVERSAMPLING), .BAUDRATE(BAUDRATE),.COUNTER_CLOCK_INPUT(COUNTER_CLOCK_INPUT),.CLOCK_REF(CLOCK_REF))rx(
+    uart_top#(.BYTESIZES(BYTESIZES), .OVERSAMPLING(OVERSAMPLING), .BAUDRATE(BAUDRATE),.COUNTER_CLOCK_INPUT(COUNTER_CLOCK_INPUT),.CLOCK_REF(CLOCK_REF))uart_top_h(
         .clock       (clock    )        ,
         .nreset      (nreset   )        ,
-        .sdata_rx_in (sdata1   )       ,
-        .valid_rx_in (valid_tx1 )        ,
-        .ready_rx_out(ready_rx )        ,
-        .data_rx_out (dataout )   
-    );
-    uart_tx #(.BYTESIZES(BYTESIZES), .OVERSAMPLING(1), .BAUDRATE(BAUDRATE),.COUNTER_CLOCK_INPUT(COUNTER_CLOCK_INPUT2),.CLOCK_REF(CLOCK_REF))tx1(
-        .clock       (clock2       ),
-        .nreset      (nreset       ),
-        .valid_tx_in (valid_tx1    ),
-        .data_tx_in  (pdata1       ),
-        .sdata_tx_out(sdata1       )
+        //pinout RX   
+        .sdata_rx_in (sdata1)             ,
+        .valid_rx_in (valid_tx1)          ,
+        .ready_rx_out(ready_rx)           ,
+        .data_rx_out (dataout)            ,
+        //pinout TX
+        .valid_tx_in (valid_tx1)          ,
+        .data_tx_in  (pdata1)             ,
+        .ready_tx_out(      )             ,
+        .sdata_tx_out(sdata1)            
     );
 
-    // uart_tx #(.BYTESIZES(BYTESIZES), .OVERSAMPLING(1), .BAUDRATE(BAUDRATE),.COUNTER_CLOCK_INPUT(COUNTER_CLOCK_INPUT2))tx2(
-    //     .clock  (clock2  ),
-    //     .nreset (nreset ),
-    //     .valid  (valid_tx1   ),
-    //     .data   (pdata1  ),
-    //     .sdata  (sdata2  )
-    // );
     initial begin
         rtime1 = 1/(2*COUNTER_CLOCK_INPUT);
         rtime2 = 1/(2*COUNTER_CLOCK_INPUT2);
@@ -53,7 +45,7 @@ module tb;
     always #5ns clock2 = ~ clock2;
     always_ff@(posedge clock)begin
         if(!nreset) pdata1 <= 120;
-        if(tx1.data_tx_in==rx.data_rx_out) pdata1 <= $random()%2**(BYTESIZES-1);
+        if(uart_top_h.data_tx_in==uart_top_h.data_rx_out) pdata1 <= $random()%2**(BYTESIZES-1);
         valid_rx <= $random%2;
     end
     assign pdata2 = pdata1;
