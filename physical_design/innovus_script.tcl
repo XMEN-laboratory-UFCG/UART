2ab98c5a9b6b63f46fd8cdc67e72e88012296525
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-10-29 23:56:46 -0300

    fix read_mmmc

--- physical_design/innovus_script.tcl
+++ physical_design/innovus_script.tcl
@@ -8,8 +8,9 @@ set_db init_lef_files {gsclib045_tech.lef gsclib045_macro.lef}
 
 set_db init_power_nets VDD
 set_db init_ground_nets VSS
-set_db init_mmmc_files Default.view
-read_mmmc Default.view
+#read_verilog ../sim/output_files/uart_netlist.v
+set init_mmmc_file uart2.view
+read_mmmc uart2.view
 read_physical -lefs {gsclib045_tech.lef gsclib045_macro.lef}
 read_netlist ../sim/outputs/uart_netlist.v -top uart_top
 init_design
